65b96937fb878266da068b17c2f5cd6d96e0632c
Ferenc Molnár <muustar@gmail.com>
2018-04-27 13:02:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/feco/toastpractice/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.feco.toastpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //toast custom
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.custom_toast_container));
+
+        TextView text = (TextView)layout.findViewById(R.id.text);
+        text.setText("Egyedi Toast");
+
+        final Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL,0,0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+
+
+
+
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "sima toast", Toast.LENGTH_LONG).show();
+                toast.show();
+            }
+        });
+    }
+}
